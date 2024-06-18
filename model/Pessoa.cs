de1dfa9692c248b89633e8d54e7054a882daa69c
Allan <135379484+AllMaciente@users.noreply.github.com>
2024-06-17 21:18:43 -0300

    add conexao def

--- model/Pessoa.cs
+++ model/Pessoa.cs
@@ -4,6 +4,7 @@ namespace Model
 {
     public class Pessoa
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Cpf { get; set; }
--- repository/Pessoa.cs
+++ repository/Pessoa.cs
@@ -1,9 +1,29 @@
 using Model;
+using MySqlConnector
 
 namespace Repo
 {
     public class RepoPessoa
     {
         public static List<Pessoa> pessoas = new List<Pessoa>();
+        static private MySqlConnaction conexao;
+        public static void InitConexao()
+        {
+            string info = "server=localhost;database=projetointegrador;user id=root;password=''"
+            conexao = new MySqlConnection(info);
+            try
+            {
+                InitConexao.Open();
+            }
+            catch
+            {
+                MessageBox.show("Impossível estabelecer conexão")
+            }
+        }
+        public static void CloseConexao()
+        {
+            conexao.Close()
+        }
+
     }
 }
